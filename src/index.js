d9721d7b74e37a6bace1ad0603f266fcc03dc8db
Amitkumarpatwa <amitpatwa80040@gmail.com>
2026-02-24 17:22:24 +0000

    correct spelling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser=require('body-parser')
 
 const {PORT}=require('./config/server.config');
 const apiRouter = require('./routes/index');
-const errorHandler = require('./utils/errorHandeler');
+const errorHandler = require('./utils/errorHandler');
 
 const app=express();
 
