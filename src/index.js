87c3da400b9fb8c91bcda1095b0e5f140ddf55dd
Amitkumarpatwa <amitpatwa80040@gmail.com>
2026-03-01 17:33:35 +0530

    Feat: Added mongoose db config

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
-const express=require('express');
-const bodyParser=require('body-parser')
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const {PORT}=require('./config/server.config');
-const apiRouter = require('./routes/index');
-const errorHandler = require('./utils/errorHandler');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app=express();
+const { PORT } = require("./config/server.config");
+const apiRouter = require("./routes/index");
+const errorHandler = require("./utils/errorHandler");
+const connectToDB = require("./config/db.config.js");
+
+const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
 // If any request comes and route start with /api we map it apiRouter
-app.use('/api',apiRouter);
+app.use("/api", apiRouter);
 
-app.get('/ping',(req,res)=>{
-    return res.json({message:"Problem service is alive"})
-})
+app.get("/ping", (req, res) => {
+  return res.json({ message: "Problem service is alive" });
+});
 
 app.use(errorHandler);
 
-app.listen(PORT,()=>{
-    console.log(`Server started at PORT: ${PORT}`);
-    
-})
+app.listen(PORT, async () => {
+  console.log(`Server started at PORT: ${PORT}`);
+
+  await connectToDB();
+  console.log("Successfully connected to db");
+
+ 
+  
+});
