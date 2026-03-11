f37ac8093093927dcf9ec8b1bd118ba502cebbb0
Amitkumarpatwa <amitpatwa80040@gmail.com>
2026-03-11 22:01:09 +0530

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const dns = require("dns");
-dns.setDefaultResultOrder("ipv4first");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
+// const dns = require("dns");
+// dns.setDefaultResultOrder("ipv4first");
+// dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -28,9 +28,9 @@ app.use(errorHandler);
 app.listen(PORT, async () => {
   console.log(`Server started at PORT: ${PORT}`);
 
-  await connectToDB();
-  console.log("Successfully connected to db");
-
- 
-  
+  try {
+    await connectToDB();
+  } catch (error) {
+    console.log("Failed to connect to database");
+  }
 });
