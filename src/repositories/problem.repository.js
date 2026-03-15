4772d6f288bec5651ac503778d00bc8b03d1bed2
Amitkumarpatwa <amitpatwa80040@gmail.com>
2026-03-15 16:06:03 +0530

    Feat: configured for file ,console and mongo

--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -1,49 +1,65 @@
 // const Problem=require('../models');
 
-const NotFound = require("../errors/notfound.error.js");
 const { Problem } = require("../models/index.js");
 
-class ProblemRepository{
-
-    async createProblem(problemData){
-        try {
-            const problem= await Problem.create({
-                title: problemData.title,
-                description: problemData.description,
-                testCases: (problemData.testCases)?problemData.testCases:[]
-            });
-            return problem;
-        } catch (error) {
-            console.log(error);
-            throw error;
-            
-        }
+class ProblemRepository {
+  async createProblem(problemData) {
+    try {
+      const problem = await Problem.create({
+        title: problemData.title,
+        description: problemData.description,
+        testCases: problemData.testCases ? problemData.testCases : [],
+      });
+      return problem;
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
+  }
 
-    async getAllProblems(){
-        try {
-            const problems = await Problem.find({});
-            return problems;
-        } catch (error) {
-            console.log(error);
-            throw error;
-        }
+  async getAllProblems() {
+    try {
+      const problems = await Problem.find({});
+      return problems;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async getProblem(id) {
+    try {
+      const problem = await Problem.findById(id);
+
+      return problem;
+    } catch (error) {
+      // console.log(error);
+
+      throw error;
+    }
+  }
+  async deleteProblem(id) {
+    try {
+      const problem = await Problem.findByIdAndDelete(id);
+      return problem;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async updateProblem(id, problemData) {
+    try {
+      const problem = await Problem.findByIdAndUpdate(id, problemData, {
+        new: true,
+        runValidators: true,
+      });
+      return problem;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 }
 
-    async getProblem(id){
-        try {
-            const problem= await Problem.findById(id);
-            
-            return problem;
-        } catch (error) {
-            if (error.name === "CastError") {
-                throw new NotFound("Problem", id);
-            }
-            console.log(error);
-            
-            throw error;
-        }
-    }
-}
-
-module.exports=ProblemRepository;
+module.exports = ProblemRepository;
--- src/services/problem.service.js
+++ src/services/problem.service.js
@@ -1,4 +1,6 @@
 const sanitizeMarkdownContent = require("../utils/markdownSanitizer.js");
+const NotFound = require("../errors/notfound.error.js");
+const logger = require("../config/logger.config.js");
 
 class ProblemService {
   constructor(problemRepository) {
@@ -8,10 +10,7 @@ class ProblemService {
     // 1, sanitized the markdown for description
     problemData.description = sanitizeMarkdownContent(problemData.description);
 
-    console.log("problem data:", problemData);
-
     const problem = await this.problemRepository.createProblem(problemData);
-    console.log("Problem created", problem);
 
     return problem;
   }
@@ -21,8 +20,76 @@ class ProblemService {
     return problems;
   }
   async getProblem(problemId) {
-    const problem = await this.problemRepository.getProblem(problemId);
-    return problem;
+    try {
+      const problem = await this.problemRepository.getProblem(problemId);
+
+      if (!problem) {
+        // this null when formate id is correct but not exits in collection 
+        logger.error(`Problem is not found ${problemId}`);
+        
+        
+        throw new NotFound("Problem", problemId);
+      }
+
+      return problem;
+    } catch (error) {
+      // logger.error(`Problem is not found ${problemId}`);
+      // console.log(error);
+      
+      
+      if (error.name === "CastError") {
+        logger.error(`Invalid problem id format ${problemId} in the database`);
+        console.log(error);
+        throw new NotFound("Problem", problemId);
+      }
+
+      throw error;
+    }
+  }
+  async deleteProblem(problemId) {
+    try {
+      const problem = await this.problemRepository.deleteProblem(problemId);
+
+      if (!problem) {
+        throw new NotFound("Problem", problemId);
+      }
+
+      return problem;
+    } catch (error) {
+      
+      if (error.name === "CastError") {
+        throw new NotFound("Problem", problemId);
+      }
+
+      throw error;
+    }
+  }
+  async updateProblem(problemId, updateData) {
+    try {
+      // sanitize description if provided
+      if (updateData.description) {
+        updateData.description = sanitizeMarkdownContent(
+          updateData.description,
+        );
+      }
+
+      const problem = await this.problemRepository.updateProblem(
+        problemId,
+        updateData,
+      );
+
+      if (!problem) {
+        throw new NotFound("Problem", problemId);
+      }
+
+      return problem;
+    } catch (error) {
+      if (error.name === "CastError") {
+        throw new NotFound("Problem", problemId);
+      }
+
+      throw error;
+    }
   }
 }
 
